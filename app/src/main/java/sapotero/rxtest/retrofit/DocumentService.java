a2f4eaf4f94f5423c99fc93e29d0aa2c2d2f6015
Сапожников Егор <esapozhnikov@n-core.ru>
2016-09-15 18:56:35 +0300

    document info

---
+++ app/src/main/java/sapotero/rxtest/retrofit/DocumentService.java
@@ -0,0 +1,16 @@
+package sapotero.rxtest.retrofit;
+
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import rx.Observable;
+import sapotero.rxtest.models.document.DocumentInfo;
+
+public interface DocumentService {
+  @GET("{document}.json")
+  Observable<DocumentInfo> getInfo(
+      @Path("document") String document,
+      @Query("login") String login,
+      @Query("auth_token") String auth_token
+  );
+}
--- app/src/main/java/sapotero/rxtest/views/activities/MainActivity.java
+++ app/src/main/java/sapotero/rxtest/views/activities/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -20,9 +21,11 @@ import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 import sapotero.rxtest.R;
 import sapotero.rxtest.models.AuthToken;
+import sapotero.rxtest.models.document.DocumentInfo;
 import sapotero.rxtest.models.documents.Document;
 import sapotero.rxtest.models.documents.Documents;
 import sapotero.rxtest.retrofit.AuthTokenService;
+import sapotero.rxtest.retrofit.DocumentService;
 import sapotero.rxtest.retrofit.DocumentsService;
 import sapotero.rxtest.utils.RecyclerItemClickListener;
 import sapotero.rxtest.views.adapters.DocumentsAdapter;
@@ -34,6 +37,19 @@ public class MainActivity extends AppCompatActivity {
   private RecyclerView rv;
   private String token;
 
+  private TextView uid;
+  private TextView md5;
+  private TextView title;
+  private TextView registration_number;
+  private TextView urgency;
+  private TextView short_description;
+  private TextView comment;
+  private TextView external_document_number;
+  private TextView receipt_date;
+
+  private TableLayout infoTable;
+
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -41,6 +57,17 @@ public class MainActivity extends AppCompatActivity {
 
     progressBar  = findViewById(R.id.progressBar);
     auth_token   = (TextView) findViewById(R.id.auth_token);
+
+    uid                      = (TextView) findViewById(R.id.uid);
+    md5                      = (TextView) findViewById(R.id.md5);
+    title                    = (TextView) findViewById(R.id.title);
+    registration_number      = (TextView) findViewById(R.id.registration_number);
+    urgency                  = (TextView) findViewById(R.id.urgency);
+    short_description        = (TextView) findViewById(R.id.short_description);
+    comment                  = (TextView) findViewById(R.id.comment);
+    external_document_number = (TextView) findViewById(R.id.external_document_number);
+    receipt_date             = (TextView) findViewById(R.id.receipt_date);
+    infoTable                = (TableLayout) findViewById(R.id.infoTable);
 
     rv           = (RecyclerView) findViewById(R.id.documentsRecycleView);
     LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
@@ -117,6 +144,8 @@ public class MainActivity extends AppCompatActivity {
 
   private void loadDocuments(){
 
+    progressBar.setVisibility(ProgressBar.VISIBLE);
+
     Retrofit retrofit = new Retrofit.Builder()
         .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
         .addConverterFactory(GsonConverterFactory.create())
@@ -131,23 +160,59 @@ public class MainActivity extends AppCompatActivity {
         .observeOn( AndroidSchedulers.mainThread() )
         .subscribe(data -> {
 
+          progressBar.setVisibility(ProgressBar.INVISIBLE);
+
           List<Document> docs = data.getDocuments();
           DocumentsAdapter adapter = new DocumentsAdapter(docs);
           rv.setAdapter(adapter);
-
-          Log.e("_", docs.get(0).getMd5() );
         });
 
   }
 
   private void showDocumentInfo(View view, int position){
+    progressBar.setVisibility(ProgressBar.VISIBLE);
+    infoTable.setVisibility(View.INVISIBLE);
+
     Log.d("_showDocumentInfo", String.valueOf(position));
     Log.d("_showDocumentInfo", String.valueOf(rv.getAdapter().getItemCount()));
 
     DocumentsAdapter rvAdapter = (DocumentsAdapter) rv.getAdapter();
-
-
     Log.d("_showDocumentInfo", rvAdapter.getItem(position).getUid() );
+
+    Retrofit retrofit = new Retrofit.Builder()
+        .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl("http://mobile.esd.n-core.ru/v3/documents/")
+        .build();
+
+    DocumentService documentService = retrofit.create( DocumentService.class );
+
+    Observable<DocumentInfo> info = documentService.getInfo(
+        rvAdapter.getItem(position).getUid(),
+        "admin",
+        token
+    );
+
+    info.subscribeOn( Schedulers.newThread() )
+        .observeOn( AndroidSchedulers.mainThread() )
+        .subscribe(data -> {
+          progressBar.setVisibility(ProgressBar.INVISIBLE);
+
+          infoTable.setVisibility(View.VISIBLE);
+
+          uid.setText( data.getUid() );
+          md5.setText( data.getMd5() );
+          title.setText( data.getTitle() );
+          registration_number.setText( data.getRegistrationNumber() );
+          urgency.setText( data.getUrgency() );
+          short_description.setText( data.getShortDescription() );
+          comment.setText( data.getComment() );
+          external_document_number.setText( data.getExternalDocumentNumber() );
+          receipt_date.setText( data.getReceiptDate() );
+
+
+
+        });
 
   }
 }
