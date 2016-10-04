22c34362d8e4b1de9015353105f27ba799181dc7
Сапожников Егор <esapozhnikov@n-core.ru>
2016-10-04 19:17:14 +0300

    Improve

--- app/src/main/java/sapotero/rxtest/views/activities/InfoActivity.java
+++ app/src/main/java/sapotero/rxtest/views/activities/InfoActivity.java
@@ -1,12 +1,25 @@
 package sapotero.rxtest.views.activities;
 
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.ProgressBar;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Objects;
 
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
@@ -16,23 +29,35 @@ import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 import sapotero.rxtest.EsdConfig;
 import sapotero.rxtest.R;
+import sapotero.rxtest.models.document.Decision;
 import sapotero.rxtest.models.document.DocumentInfo;
 import sapotero.rxtest.retrofit.DocumentService;
 import sapotero.rxtest.views.adapters.DocumentsAdapter;
+import sapotero.rxtest.views.adapters.TabPagerAdapter;
+import sapotero.rxtest.views.fragments.InfoCardFragment;
 
-public class InfoActivity extends AppCompatActivity {
+public class InfoActivity extends AppCompatActivity implements InfoCardFragment.OnFragmentInteractionListener {
 
-  private static TextView uid;
-  private static TextView sort_key;
-  private static TextView title;
-  private static TextView registration_number;
-  private static TextView urgency;
-  private static TextView short_description;
-  private static TextView comment;
-  private static TextView external_document_number;
-  private static TextView receipt_date;
-  private static TextView signer;
-  private static TextView organisation;
+  private  TextView uid;
+  private  TextView sort_key;
+  private  TextView title;
+  private  TextView registration_number;
+  private  TextView urgency;
+  private  TextView short_description;
+  private  TextView comment;
+  private  TextView external_document_number;
+  private  TextView receipt_date;
+  private  TextView signer;
+  private  TextView organisation;
+
+  private  TableLayout decision_table;
+  private  TableLayout route_table;
+  private  TableRow decision_row;
+  private  TableRow route_row;
+
+  private static byte[] CARD;
+
+  private  WebView info_card;
 
   private static String TOKEN    = "";
   private static String LOGIN    = "";
@@ -56,6 +81,14 @@ public class InfoActivity extends AppCompatActivity {
     external_document_number = (TextView) findViewById(R.id.external_document_number);
     receipt_date             = (TextView) findViewById(R.id.receipt_date);
 
+    decision_table = (TableLayout) findViewById(R.id.decision_table);
+    decision_row = (TableRow) findViewById(R.id.decision_row);
+
+    route_table = (TableLayout) findViewById(R.id.route_table);
+    route_row = (TableRow) findViewById(R.id.route_row);
+
+//    info_card = (WebView) findViewById(R.id.info_card);
+
     loader  = findViewById(R.id.loader);
     loader.setVisibility(ProgressBar.VISIBLE);
 
@@ -64,57 +97,200 @@ public class InfoActivity extends AppCompatActivity {
 
     Bundle extras = getIntent().getExtras();
 
-    if (extras != null) {
-      LOGIN    = extras.getString( EsdConfig.LOGIN);
-      TOKEN    = extras.getString( EsdConfig.TOKEN);
-      PASSWORD = extras.getString( EsdConfig.PASSWORD);
-      POSITION = extras.getInt(String.valueOf(EsdConfig.POSITION));
 
-      Log.d( "__INTENT", LOGIN );
-      Log.d( "__INTENT", PASSWORD );
-      Log.d( "__INTENT", TOKEN );
+    // Get the ViewPager and set it's PagerAdapter so that it can display items
+    ViewPager viewPager = (ViewPager) findViewById(R.id.tab_main);
+    viewPager.setAdapter( new TabPagerAdapter(getSupportFragmentManager(), InfoActivity.this) );
 
-      DocumentsAdapter rvAdapter = (DocumentsAdapter) MainActivity.rv.getAdapter();
 
-      Retrofit retrofit = new Retrofit.Builder()
-        .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-        .addConverterFactory(GsonConverterFactory.create())
-        .baseUrl("http://mobile.esd.n-core.ru/v3/documents/")
-        .build();
+    // Give the TabLayout the ViewPager
+    TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+    tabLayout.setupWithViewPager(viewPager);
+    tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+      @Override
+      public void onTabSelected(TabLayout.Tab tab) {
+        if ( Objects.equals( tab.getPosition(), 1 ) ) {
+          WebView webView = (WebView) findViewById(R.id.web_infocard);
+          try {
+            webView.loadData( new String(CARD, "UTF-8"), "text/html; charset=utf-8", "utf-8" );
+          } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+          }
+        }
+      }
 
-      DocumentService documentService = retrofit.create( DocumentService.class );
+      @Override
+      public void onTabUnselected(TabLayout.Tab tab) {
 
-      Observable<DocumentInfo> info = documentService.getInfo(
-        rvAdapter.getItem(POSITION).getUid(),
-        LOGIN,
-        TOKEN
-      );
+      }
 
-      info.subscribeOn( Schedulers.newThread() )
-        .observeOn( AndroidSchedulers.mainThread() )
-        .subscribe(
-          data -> {
-            loader.setVisibility(ProgressBar.INVISIBLE);
+      @Override
+      public void onTabReselected(TabLayout.Tab tab) {
 
-            title.setText( data.getTitle() );
-            uid.setText( data.getUid() );
-            sort_key.setText( data.getSortKey().toString() );
-            registration_number.setText(data.getRegistrationNumber());
-            urgency.setText( data.getUrgency() );
-            short_description.setText( data.getShortDescription() );
-            comment.setText( data.getComment() );
-            external_document_number.setText(data.getExternalDocumentNumber());
-            receipt_date.setText(data.getReceiptDate());
+      }
+    });
 
-            signer.setText( data.getSigner().getName() );
-            organisation.setText( data.getSigner().getOrganisation() );
-          },
-          error -> {
-            loader.setVisibility(ProgressBar.INVISIBLE);
-              Log.d( "_ERROR", error.getMessage() );
-              Toast.makeText( this, error.getMessage(), Toast.LENGTH_SHORT).show();
-          });
+    if (extras != null){
+        LOGIN    = extras.getString( EsdConfig.LOGIN);
+        TOKEN    = extras.getString( EsdConfig.TOKEN);
+        PASSWORD = extras.getString( EsdConfig.PASSWORD);
+        POSITION = extras.getInt(String.valueOf(EsdConfig.POSITION));
+
+        Log.d( "__INTENT", LOGIN );
+        Log.d( "__INTENT", PASSWORD );
+        Log.d( "__INTENT", TOKEN );
+
+        DocumentsAdapter rvAdapter = (DocumentsAdapter) MainActivity.rv.getAdapter();
+
+        Retrofit retrofit = new Retrofit.Builder()
+          .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+          .addConverterFactory(GsonConverterFactory.create())
+          .baseUrl("http://mobile.esd.n-core.ru/v3/documents/")
+          .build();
+
+        DocumentService documentService = retrofit.create( DocumentService.class );
+
+        Observable<DocumentInfo> info = documentService.getInfo(
+          rvAdapter.getItem(POSITION).getUid(),
+          LOGIN,
+          TOKEN
+        );
+
+        info.subscribeOn( Schedulers.newThread() )
+          .observeOn( AndroidSchedulers.mainThread() )
+          .subscribe(
+            data -> {
+              loader.setVisibility(ProgressBar.INVISIBLE);
+
+              title.setText( data.getTitle() );
+              uid.setText( data.getUid() );
+  //            sort_key.setText( data.getSortKey() );
+              registration_number.setText(data.getRegistrationNumber());
+              urgency.setText( data.getUrgency() );
+              short_description.setText( data.getShortDescription() );
+              external_document_number.setText(data.getExternalDocumentNumber());
+              receipt_date.setText(data.getReceiptDate());
+              comment.setText( data.getComment() );
+
+              signer.setText( data.getSigner().getName() );
+              organisation.setText( data.getSigner().getOrganisation() );
+
+              if ( data.getDecisions().size() >= 1 ){
+                Log.d( "__ERROR", String.valueOf(data.getDecisions().size()));
+                createDecisionTableHeader();
+
+                for (Decision decision: data.getDecisions()) {
+                  addRowToDecisionTable( decision );
+                }
+              } else {
+                decision_row.setVisibility(TableRow.INVISIBLE);
+              }
+
+              CARD = Base64.decode( data.getInfoCard().getBytes(), Base64.DEFAULT );
+
+            },
+            error -> {
+              loader.setVisibility(ProgressBar.INVISIBLE);
+                Log.d( "_ERROR", error.getMessage() );
+                Toast.makeText( this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            });
     }
   }
 
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.info, menu);
+
+    menu.add(0, 0, 0, "Информационная карточка")
+      .setIcon(android.R.drawable.ic_dialog_info)
+      .setOnMenuItemClickListener(
+        item -> {
+
+          try {
+            InfoActivity.this.showInfoCard();
+          } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+          }
+
+          return true;
+      })
+      .setShowAsAction( MenuItem.SHOW_AS_ACTION_ALWAYS);
+
+    return true;
+  }
+
+  public void createDecisionTableHeader(){
+    TableRow header = new TableRow(this);
+
+    TextView field_num = new TextView(this);
+    field_num.setText(" № ");
+    field_num.setTextColor( Color.BLACK );
+    header.addView(field_num);
+
+    TextView field_type = new TextView(this);
+    field_type.setText(" Тип ");
+    field_type.setTextColor( Color.BLACK );
+    header.addView(field_type);
+
+    TextView field_date = new TextView(this);
+    field_date.setText(" Дата ");
+    field_date.setTextColor( Color.BLACK );
+    header.addView(field_date);
+
+    TextView field_resolution = new TextView(this);
+    field_resolution.setText(" Резолюция ");
+    field_resolution.setTextColor( Color.BLACK );
+    header.addView(field_resolution);
+
+    TextView field_status = new TextView(this);
+    field_status.setText(" Статус ");
+    field_status.setTextColor( Color.BLACK );
+    header.addView(field_status);
+
+    decision_table.addView(header);
+  }
+
+  public void addRowToDecisionTable(Decision data){
+
+    TableRow row = new TableRow(this);
+
+    TextView field_num = new TextView(this);
+    field_num.setText( data.getId() );
+    row.addView(field_num);
+
+    TextView field_type = new TextView(this);
+    field_type.setText( data.getSigner() );
+    row.addView(field_type);
+
+    TextView field_date = new TextView(this);
+    field_date.setText( data.getDate() );
+    row.addView(field_date);
+
+    TextView field_resolution = new TextView(this);
+    field_resolution.setText( data.getLetterhead() );
+    row.addView(field_resolution);
+
+    TextView field_status = new TextView(this);
+    field_status.setText(data.getApproved() ? "Утверждена" : "Не утверждена" );
+    row.addView(field_status);
+
+    decision_table.addView(row);
+
+  }
+
+  private void showInfoCard() throws UnsupportedEncodingException {
+//    AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+
+//    WebView webView =
+//    webView.loadData( new String(CARD, "UTF-8"), "text/html; charset=utf-8", "utf-8" );
+
+//    dialog.setView( webView )
+//      .setPositiveButton( "Ok", null )
+//      .show();
+  }
+
+  @Override
+  public void onFragmentInteraction(Uri uri) {
+
+  }
 }
--- app/src/main/java/sapotero/rxtest/views/activities/MainActivity.java
+++ app/src/main/java/sapotero/rxtest/views/activities/MainActivity.java
@@ -7,12 +7,21 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.birbit.android.jobqueue.JobManager;
+import com.birbit.android.jobqueue.config.Configuration;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -26,7 +35,9 @@ import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 import sapotero.rxtest.EsdConfig;
+import sapotero.rxtest.Jobs.GetDocumentInfoJob;
 import sapotero.rxtest.R;
+import sapotero.rxtest.events.GetDocumentInfoEvent;
 import sapotero.rxtest.models.documents.Document;
 import sapotero.rxtest.models.documents.Documents;
 import sapotero.rxtest.retrofit.DocumentsService;
@@ -35,24 +46,32 @@ import sapotero.rxtest.views.adapters.DocumentsAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static String TOKEN    = "";
-    private static String LOGIN    = "";
-    private static String PASSWORD = "";
+  static JobManager jobManager;
+
+  private static String TOKEN    = "";
+  private static String LOGIN    = "";
+  private static String PASSWORD = "";
+
+  private static Integer total = 0;
+
 
   private static MainActivity context;
 
   private static View progressBar;
-    public static RecyclerView rv;
+  public static  RecyclerView rv;
 
-    private static Spinner DOCUMENT_TYPE_SELECTOR;
+  private static Spinner DOCUMENT_TYPE_SELECTOR;
+  private static Spinner JOURNAL_TYPE_SELECTOR;
+  private static Spinner ORGANIZATION_SELECTOR;
 
-  final OkHttpClient okHttpClient = new OkHttpClient.Builder()
-      .readTimeout(60, TimeUnit.SECONDS)
-      .connectTimeout(60, TimeUnit.SECONDS)
-      .addNetworkInterceptor(
-          new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.HEADERS)
-      )
-      .build();
+
+  public final OkHttpClient okHttpClient = new OkHttpClient.Builder()
+    .readTimeout(60, TimeUnit.SECONDS)
+    .connectTimeout(60, TimeUnit.SECONDS)
+    .addNetworkInterceptor(
+        new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.HEADERS)
+    )
+    .build();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -79,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    // устанавливаем тип документов
+    // устанавливаем статус документов
     DOCUMENT_TYPE_SELECTOR = (Spinner) findViewById(R.id.DOCUMENT_TYPE);
     ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.DOCUMENT_TYPES, android.R.layout.simple_spinner_item);
     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -93,9 +112,48 @@ public class MainActivity extends AppCompatActivity {
 
       @Override
       public void onNothingSelected(AdapterView<?> adapterView) {
-
       }
     });
+
+    // устанавливаем тип документов
+    JOURNAL_TYPE_SELECTOR = (Spinner) findViewById(R.id.JOURNAL_TYPE);
+    ArrayAdapter<CharSequence> journal_adapter = ArrayAdapter.createFromResource(this, R.array.JOURNAL_TYPES, android.R.layout.simple_spinner_item);
+    journal_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    JOURNAL_TYPE_SELECTOR.setAdapter(journal_adapter);
+
+    JOURNAL_TYPE_SELECTOR.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+      @Override
+      public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+        int spinner_pos = JOURNAL_TYPE_SELECTOR.getSelectedItemPosition();
+        String[] document_type = getResources().getStringArray(R.array.JOURNAL_TYPES_VALUE);
+        String type = String.valueOf(document_type[spinner_pos]);
+
+        Toast.makeText(context, type, Toast.LENGTH_SHORT).show();
+      }
+
+      @Override
+      public void onNothingSelected(AdapterView<?> adapterView) {
+      }
+    });
+
+    // устанавливаем тип документов
+    ORGANIZATION_SELECTOR = (Spinner) findViewById(R.id.ORGANIZATION);
+    ArrayAdapter<CharSequence> organization_adapter = ArrayAdapter.createFromResource(this, R.array.ORGANIZATIONS, android.R.layout.simple_spinner_item);
+    organization_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    ORGANIZATION_SELECTOR.setAdapter(organization_adapter);
+
+    ORGANIZATION_SELECTOR.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+      @Override
+      public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+      }
+
+      @Override
+      public void onNothingSelected(AdapterView<?> adapterView) {
+      }
+    });
+
+
+
 
 
     rv = (RecyclerView) findViewById(R.id.documentsRecycleView);
@@ -111,6 +169,14 @@ public class MainActivity extends AppCompatActivity {
     rv.setLayoutManager(gridLayoutManager);
     progressBar.setVisibility(ProgressBar.INVISIBLE);
 
+    Configuration.Builder builder = new Configuration.Builder(this)
+      .minConsumerCount(1)//always keep at least one consumer alive
+      .maxConsumerCount(3)//up to 3 consumers at a time
+      .loadFactor(3)//3 jobs per consumer
+      .consumerKeepAlive(120);//wait 2 minute
+
+    jobManager = new JobManager(builder.build());
+
 
   }
 
@@ -119,12 +185,12 @@ public class MainActivity extends AppCompatActivity {
     progressBar.setVisibility(ProgressBar.VISIBLE);
 
     Retrofit retrofit = new Retrofit.Builder()
-        .client(okHttpClient)
-        .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-        .addCallAdapterFactory(RxErrorHandlingCallAdapterFactory.create())
-        .addConverterFactory(GsonConverterFactory.create())
-        .baseUrl("http://mobile.esd.n-core.ru/v3/")
-        .build();
+      .client(okHttpClient)
+      .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+      .addCallAdapterFactory(RxErrorHandlingCallAdapterFactory.create())
+      .addConverterFactory(GsonConverterFactory.create())
+      .baseUrl("http://mobile.esd.n-core.ru/v3/")
+      .build();
 
     DocumentsService documentsService = retrofit.create( DocumentsService.class );
 
@@ -135,21 +201,23 @@ public class MainActivity extends AppCompatActivity {
     Observable<Documents> documents = documentsService.getDocuments( LOGIN, TOKEN, type);
 
     documents.subscribeOn( Schedulers.newThread() )
-        .observeOn( AndroidSchedulers.mainThread() )
-        .subscribe(
-            data -> {
-                progressBar.setVisibility(ProgressBar.INVISIBLE);
+      .observeOn( AndroidSchedulers.mainThread() )
+      .subscribe(
+        data -> {
+          progressBar.setVisibility(ProgressBar.INVISIBLE);
 
-                List<Document> docs = data.getDocuments();
-                DocumentsAdapter adapter = new DocumentsAdapter(this, docs);
-                rv.setAdapter(adapter);
-            },
-            error -> {
-                Log.d( "_ERROR", error.getMessage() );
-                progressBar.setVisibility(ProgressBar.INVISIBLE);
+          List<Document> docs = data.getDocuments();
+          total = docs.size();
 
-                Toast.makeText(this, error.getMessage(), Toast.LENGTH_SHORT).show();
-            });
+          DocumentsAdapter adapter = new DocumentsAdapter(this, docs);
+          rv.setAdapter(adapter);
+        },
+        error -> {
+          Log.d( "_ERROR", error.getMessage() );
+          progressBar.setVisibility(ProgressBar.INVISIBLE);
+
+          Toast.makeText(this, error.getMessage(), Toast.LENGTH_SHORT).show();
+        });
 
   }
 
@@ -162,5 +230,50 @@ public class MainActivity extends AppCompatActivity {
     intent.putExtra(String.valueOf(EsdConfig.POSITION),    position );
 
     context.startActivity(intent);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.info, menu);
+
+    menu.add(0, 0, 0, "Загрузка документов")
+      .setIcon(android.R.drawable.ic_menu_info_details)
+      .setOnMenuItemClickListener(
+        item -> {
+
+          MainActivity.process();
+
+          return true;
+        })
+      .setShowAsAction( MenuItem.SHOW_AS_ACTION_ALWAYS);
+
+    return true;
+  }
+
+  private static void process() {
+    Toast.makeText( context, "Total: "+total.toString(), Toast.LENGTH_SHORT).show();
+    try {
+//      jobManager.addJobInBackground( new GetDocumentInfoJob("1") );
+      jobManager.addJobInBackground( new GetDocumentInfoJob("1"));
+    } catch ( Exception e){
+      Log.d( "_ERROR", e.toString() );
+    }
+
+  }
+  @Override
+  public void onStart() {
+    super.onStart();
+    EventBus.getDefault().register(this);
+  }
+
+  @Override
+  public void onStop() {
+    EventBus.getDefault().unregister(this);
+    super.onStop();
+  }
+
+  @Subscribe(threadMode = ThreadMode.BACKGROUND)
+  public void onMessageEvent(GetDocumentInfoEvent event) {
+    Toast.makeText(context, event.message, Toast.LENGTH_SHORT).show();
   }
 }
