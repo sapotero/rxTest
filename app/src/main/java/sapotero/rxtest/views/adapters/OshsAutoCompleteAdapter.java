46255c0caeea87f390440f4e5cac8df61fce667e
Сапожников Егор <esapozhnikov@n-core.ru>
2016-10-27 13:32:13 +0300

    autocomplete field

---
+++ app/src/main/java/sapotero/rxtest/views/adapters/OshsAutoCompleteAdapter.java
@@ -0,0 +1,8 @@
+package sapotero.rxtest.views.adapters;
+
+/**
+ * Created by sapotero on 26.10.16.
+ */
+
+public class OshsAutoCompleteAdapter {
+}
---
+++ app/src/main/java/sapotero/rxtest/views/views/DelayAutoCompleteTextView.java
@@ -0,0 +1,8 @@
+package sapotero.rxtest.views.views;
+
+/**
+ * Created by sapotero on 26.10.16.
+ */
+
+public class DelayAutoCompleteTextView {
+}
