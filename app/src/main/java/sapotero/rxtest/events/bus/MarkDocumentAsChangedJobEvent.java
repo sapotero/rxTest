d88fdffb37bcbd6e37b5769f8d70949b2f3f7e27
Сапожников Егор <esapozhnikov@n-core.ru>
2017-01-26 19:12:17 +0300

    remove unused code

--- app/src/main/java/sapotero/rxtest/events/bus/MarkDocumentAsChangedJobEvent.java
+++
@@ -1,9 +0,0 @@
-package sapotero.rxtest.events.bus;
-
-public class MarkDocumentAsChangedJobEvent {
-  public final String uid;
-
-  public MarkDocumentAsChangedJobEvent(String uid) {
-    this.uid = uid;
-  }
-}
