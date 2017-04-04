f79285c4743c1b0ed270f01fd26d4a702e5a4012
Сапожников Егор <esapozhnikov@n-core.ru>
2017-04-04 13:36:12 +0300

    save and sign for decision

---
+++ app/src/main/java/sapotero/rxtest/events/view/InvalidateDecisionSpinnerEvent.java
@@ -0,0 +1,9 @@
+package sapotero.rxtest.events.view;
+
+public class InvalidateDecisionSpinnerEvent {
+  public String uid;
+
+  public InvalidateDecisionSpinnerEvent(String uid) {
+    this.uid = uid;
+  }
+}
