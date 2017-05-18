0642ccf86a681728ec80c645a3e6bcdecedbca6a
Gennady Petukhov <gpetukhov@n-core.ru>
2017-05-18 15:15:05 +0300

    Add performer mapper

---
+++ app/src/main/java/sapotero/rxtest/db/mapper/Mapper.java
@@ -0,0 +1,7 @@
+package sapotero.rxtest.db.mapper;
+
+// Maps between model (pojo) (M) and requery entity (E)
+public interface Mapper<M, E> {
+  E toEntity(M model);
+  M toModel(E entity);
+}
---
+++ app/src/main/java/sapotero/rxtest/db/mapper/PerformerMapper.java
@@ -0,0 +1,42 @@
+package sapotero.rxtest.db.mapper;
+
+import sapotero.rxtest.db.requery.models.decisions.RPerformerEntity;
+import sapotero.rxtest.retrofit.models.document.Performer;
+
+// Maps between Performer and RPerformerEntity
+public class PerformerMapper implements Mapper<Performer, RPerformerEntity> {
+
+  @Override
+  public RPerformerEntity toEntity(Performer model) {
+    RPerformerEntity entity = new RPerformerEntity();
+
+    entity.setNumber(model.getNumber());
+    entity.setPerformerId(model.getPerformerId());
+    entity.setPerformerType(model.getPerformerType());
+    entity.setPerformerText(model.getPerformerText());
+    entity.setPerformerGender(model.getPerformerGender());
+    entity.setOrganizationText(model.getOrganizationText());
+    entity.setIsOriginal(model.getIsOriginal());
+    entity.setIsResponsible(model.getIsResponsible());
+    entity.setIsOrganization(model.getOrganization());
+
+    return entity;
+  }
+
+  @Override
+  public Performer toModel(RPerformerEntity entity) {
+    Performer model = new Performer();
+
+    model.setNumber(entity.getNumber());
+    model.setPerformerId(entity.getPerformerId());
+    model.setPerformerType(entity.getPerformerType());
+    model.setPerformerText(entity.getPerformerText());
+    model.setPerformerGender(entity.getPerformerGender());
+    model.setOrganizationText(entity.getOrganizationText());
+    model.setIsOriginal(entity.isIsOriginal());
+    model.setIsResponsible(entity.isIsResponsible());
+    model.setOrganization(entity.isIsOrganization());
+
+    return model;
+  }
+}
