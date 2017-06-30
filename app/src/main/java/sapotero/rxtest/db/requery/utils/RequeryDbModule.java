d16e0bbd9daf02012c947293c4128dfba3c5c19a
Сапожников Егор <esapozhnikov@n-core.ru>
2017-06-30 18:12:12 +0300

    version change

--- app/src/main/java/sapotero/rxtest/db/requery/utils/RequeryDbModule.java
+++ app/src/main/java/sapotero/rxtest/db/requery/utils/RequeryDbModule.java
@@ -26,7 +26,7 @@ public final class RequeryDbModule {
   @Provides
   @DataScope
   SingleEntityStore<Persistable> provideDatabase(Context context) {
-    DatabaseSource source = new DatabaseSource(context, Models.DEFAULT, 11);
+    DatabaseSource source = new DatabaseSource(context, Models.DEFAULT, 12);
 
     EntityModel model = Models.DEFAULT;
 
