6d60dd3eba024b8feffb1a66b5bed98258fa3c19
Сапожников Егор <esapozhnikov@n-core.ru>
2017-07-04 17:33:38 +0300

    add field to db && add settings

--- app/src/main/java/sapotero/rxtest/db/requery/models/RDocument.java
+++ app/src/main/java/sapotero/rxtest/db/requery/models/RDocument.java
@@ -85,6 +85,10 @@ public abstract class RDocument implements Persistable {
 //  @Index("processed_index")
   Boolean processed;
 
+  // resolved https://tasks.n-core.ru/browse/MVDESD-13232
+  // удалять обработанные за период текущая дата - Срок хранения ЭО в обработанных документах
+  String processedDate;
+
 
   // из папки обработанное
 //  @Index("fromProcessedFolder_index")
