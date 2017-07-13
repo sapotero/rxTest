0aafafda7e0564b23aab8801333116ab6579d29c
Сапожников Егор <esapozhnikov@n-core.ru>
2017-07-13 19:50:27 +0300

    version change

--- app/src/main/java/sapotero/rxtest/views/adapters/pagination/PagingListener.java
+++
@@ -1,8 +0,0 @@
-package sapotero.rxtest.views.adapters.pagination;
-
-import java.util.List;
-import rx.Observable;
-
-public interface PagingListener<T> {
-  Observable<List<T>> onNextPage(int offset);
-}
