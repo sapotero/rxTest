785ed6658928bb17e4258c27bac3de618da19950
Сапожников Егор <esapozhnikov@n-core.ru>
2017-07-26 14:08:25 +0300

    wrong interraction with swipe layout

--- app/src/main/java/sapotero/rxtest/views/fragments/InfoCardDocumentsFragment.java
+++ app/src/main/java/sapotero/rxtest/views/fragments/InfoCardDocumentsFragment.java
@@ -5,10 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.FileProvider;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -86,7 +84,6 @@ public class InfoCardDocumentsFragment extends Fragment implements AdapterView.O
   @BindView(R.id.deleted_image) FrameLayout deletedImage;
   @BindView(R.id.broken_image) FrameLayout broken_image;
   @BindView(R.id.info_card_pdf_reload) Button reloadImageButton;
-  @BindView(R.id.swiperefresh) SwipeRefreshLayout swipe;
 
   @BindView(R.id.info_card_pdf_no_files) TextView no_files;
   @BindView(R.id.info_card_pdf_wrapper)  FrameLayout pdf_wrapper;
@@ -131,10 +128,6 @@ public class InfoCardDocumentsFragment extends Fragment implements AdapterView.O
     return view;
   }
 
-  private void refresh() {
-    swipe.setRefreshing(false);
-  }
-
   private void initSubscription() {
     directionSub
       .buffer( 500, TimeUnit.MILLISECONDS )
@@ -166,9 +159,6 @@ public class InfoCardDocumentsFragment extends Fragment implements AdapterView.O
               if ( positions.get(0) == 1.0f ){
                 getNextImage();
               }
-
-              swipe.setRefreshing(true);
-              ( new Handler()).postDelayed(this::refresh, 1000);
             }
 
 
