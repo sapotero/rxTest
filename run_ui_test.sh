a6a57a746b03c24823f30ab5c7cd31aacdcc6962
Сапожников Егор <esapozhnikov@n-core.ru>
2017-07-28 13:39:06 +0300

    fix ci config

--- run_ui_test.sh
+++ run_ui_test.sh
@@ -2,6 +2,6 @@ PWD=$(echo "app/build/outputs/apk")
 FILENAME=$(ls $PWD | head -n 1)
 FILEPATH=$(echo "${PWD}/${FILENAME}")
 
-adb push "#{FILEPATH}/#{FILENAME}" /data/local/tmp/sapotero.rxtest.test
+adb push "${FILEPATH}" /data/local/tmp/sapotero.rxtest.test
 adb shell pm install -r "/data/local/tmp/sapotero.rxtest.test"
 adb shell am instrument -w -r   -e debug false -e class sapotero.rxtest.views.activities.SettingsActivityTest sapotero.rxtest.test/
