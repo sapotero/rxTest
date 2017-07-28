3249cbc91044e439493a36f40130161a5467d1a0
Сапожников Егор <esapozhnikov@n-core.ru>
2017-07-28 16:33:34 +0300

    slow speed fix

--- run_ui_test.sh
+++ run_ui_test.sh
@@ -3,5 +3,5 @@ FILENAME=$(ls $PWD | head -n 1)
 FILEPATH=$(echo "${PWD}/${FILENAME}")
 
 adb push "${FILEPATH}" /data/local/tmp/sapotero.rxtest.test
-adb shell pm install -r "/data/local/tmp/sapotero.rxtest.test"
+adb shell pm install -r "/data/local/tmp/sapotero.rxtest"
 adb shell am instrument -w -r   -e debug false -e class sapotero.rxtest.views.activities.SettingsActivityTest sapotero.rxtest.test/
