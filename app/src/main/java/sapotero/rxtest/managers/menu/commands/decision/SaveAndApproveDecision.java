ae90bdb7b60b7ed2610071ac3f80204c29aef0d3
Gennady Petukhov <gpetukhov@n-core.ru>
2017-08-11 15:12:30 +0300

    Set Returned label if returned after SaveAndApproveDecision

--- app/src/main/java/sapotero/rxtest/managers/menu/commands/decision/SaveAndApproveDecision.java
+++ app/src/main/java/sapotero/rxtest/managers/menu/commands/decision/SaveAndApproveDecision.java
@@ -4,22 +4,19 @@ import com.google.gson.Gson;
 
 import org.greenrobot.eventbus.EventBus;
 
-import java.util.Objects;
+import java.util.Collections;
+import java.util.List;
 
-import io.requery.query.Tuple;
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
-import sapotero.rxtest.db.requery.models.RDocumentEntity;
-import sapotero.rxtest.db.requery.models.decisions.RDecisionEntity;
+import sapotero.rxtest.events.document.ForceUpdateDocumentEvent;
 import sapotero.rxtest.events.view.InvalidateDecisionSpinnerEvent;
 import sapotero.rxtest.events.view.ShowNextDocumentEvent;
 import sapotero.rxtest.managers.menu.commands.DecisionCommand;
 import sapotero.rxtest.managers.menu.utils.CommandParams;
 import sapotero.rxtest.retrofit.models.document.Decision;
 import sapotero.rxtest.retrofit.models.v2.DecisionError;
-import sapotero.rxtest.utils.memory.fields.FieldType;
-import sapotero.rxtest.utils.memory.fields.LabelType;
 import timber.log.Timber;
 
 public class SaveAndApproveDecision extends DecisionCommand {
@@ -34,10 +31,9 @@ public class SaveAndApproveDecision extends DecisionCommand {
 
   @Override
   public void execute() {
+    saveOldLabelValues(); // Must be before queueManager.add(this), because old label values are stored in params
     queueManager.add(this);
     updateLocal();
-
-    setSyncLabelInMemory();
     setAsProcessed();
   }
 
@@ -55,47 +51,17 @@ public class SaveAndApproveDecision extends DecisionCommand {
   private void updateLocal() {
     Timber.tag(TAG).e("updateLocal %s", new Gson().toJson( getParams() ));
 
-    Integer count = dataStore
-      .update(RDecisionEntity.class)
-      .set(RDecisionEntity.TEMPORARY, true)
-      .where(RDecisionEntity.UID.eq( getParams().getDecisionModel().getId() ))
-      .get().value();
+    setDecisionTemporary();
 
-    Timber.tag(TAG).i( "updateLocal: %s", count );
+    setChangedInDb();
 
-    Tuple red = dataStore
-      .select(RDecisionEntity.RED)
-      .where(RDecisionEntity.UID.eq(getParams().getDecisionModel().getId()))
-      .get().firstOrNull();
-
-    dataStore
-      .update(RDocumentEntity.class)
-      .set(RDocumentEntity.CHANGED, true)
-      .where(RDocumentEntity.UID.eq( getParams().getDocument() ))
-      .get()
-      .value();
-
-    Timber.tag(TAG).e("-------- %s %s", getParams().getDecisionModel().getSignerId(), getParams().getCurrentUserId());
-
-    if (
-        signerIsCurrentUser()
-        // или если подписывающий министр
-        || ( red != null && red.get(0).equals(true) )
-      ) {
-      dataStore
-        .update(RDocumentEntity.class)
-        .set(RDocumentEntity.PROCESSED, true)
-        .where(RDocumentEntity.UID.eq(  getParams().getDocument() ))
-        .get()
-        .value();
-
-      store.process(
-        store.startTransactionFor(  getParams().getDocument() )
-          .setLabel(LabelType.SYNC)
-          .setField(FieldType.PROCESSED, true)
-      );
-
+    if ( isActiveOrRed() ) {
+      startProcessedOperationInMemory();
+      startProcessedOperationInDb();
       EventBus.getDefault().post( new ShowNextDocumentEvent( true, getParams().getDocument() ));
+
+    } else {
+      setSyncLabelInMemory();
     }
 
     EventBus.getDefault().post( new InvalidateDecisionSpinnerEvent( getParams().getDecisionModel().getId() ));
@@ -120,14 +86,48 @@ public class SaveAndApproveDecision extends DecisionCommand {
         .observeOn( AndroidSchedulers.mainThread() )
         .subscribe(
           data -> {
-            onSuccess( data, false, true );
-            removeSyncChanged();
+            if ( notEmpty( data.getErrors() ) ) {
+              sendErrorCallback( "error" );
+              finishOnError( data.getErrors() );
+
+            } else {
+              if ( isActiveOrRed() ) {
+                finishProcessedOperationOnSuccess();
+              } else {
+                removeSyncChanged();
+                queueManager.setExecutedRemote(this);
+              }
+
+              checkCreatorAndSignerIsCurrentUser(data);
+            }
           },
-          error -> onError( error.getLocalizedMessage(), true )
+
+          error -> {
+            String errorMessage = error.getLocalizedMessage();
+
+            Timber.tag(TAG).i("error: %s", errorMessage);
+
+            sendErrorCallback( errorMessage );
+
+            if ( settings.isOnline() ) {
+              finishOnError( Collections.singletonList( errorMessage ) );
+            }
+          }
         );
 
     } else {
-      onError( SIGN_ERROR_MESSAGE, true );
+      sendErrorCallback( SIGN_ERROR_MESSAGE );
+      finishOnError( Collections.singletonList( SIGN_ERROR_MESSAGE ) );
     }
+  }
+
+  private void finishOnError(List<String> errors) {
+    if ( isActiveOrRed() ) {
+      finishRejectedProcessedOperationOnError( errors );
+    } else {
+      finishOperationWithoutProcessedOnError( errors );
+    }
+
+    EventBus.getDefault().post( new ForceUpdateDocumentEvent( getParams().getDocument() ));
   }
 }
