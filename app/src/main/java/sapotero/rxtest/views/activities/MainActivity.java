13ba6931c94fdbea4bbd1df4cedb7518f4994eab
Gennady Petukhov <gpetukhov@n-core.ru>
2017-08-29 16:53:38 +0300

    Fix DataLoaderManager

--- app/src/main/java/sapotero/rxtest/views/activities/MainActivity.java
+++ app/src/main/java/sapotero/rxtest/views/activities/MainActivity.java
@@ -408,7 +408,7 @@ public class MainActivity extends AppCompatActivity implements MenuBuilder.Callb
   }
 
   private void updateByStatus() {
-    dataLoader.updateByCurrentStatus( menuBuilder.getItem(), null, false);
+    dataLoader.updateByCurrentStatus( menuBuilder.getItem(), null);
     Toast.makeText(this, "Обновление данных...", Toast.LENGTH_SHORT).show();
   }
 
