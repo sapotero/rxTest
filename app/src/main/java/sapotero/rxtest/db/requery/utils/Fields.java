df2aee3cde9bbb96d3e0f5a5b8ce9df7a353d66f
Gennady Petukhov <gpetukhov@n-core.ru>
2017-09-13 18:36:16 +0300

    Remove unused code from Fields class

--- app/src/main/java/sapotero/rxtest/db/requery/utils/Fields.java
+++ app/src/main/java/sapotero/rxtest/db/requery/utils/Fields.java
@@ -2,36 +2,30 @@ package sapotero.rxtest.db.requery.utils;
 
 import java.util.Objects;
 
-import timber.log.Timber;
-
 public class Fields {
 
   public enum Journal {
-    ALL_JOURNALS              (  0, "90", "Все документы",        " ", ""),
-    CITIZEN_REQUESTS          (  1, "01", "Обращения граждан",    "Обращение граждан",     "Вам поступило "),
-    INCOMING_DOCUMENTS        (  2, "02", "Входящие документы",   "Входящий документ",     "Вам поступил "),
-    INCOMING_ORDERS           (  3, "03", "НПА",                  "НПА",                   "Вам поступил "),
-    ORDERS                    (  4, "04", "Приказы старые",       "Приказ старый",         "Вам поступил "),
-    OUTGOING_DOCUMENTS        (  5, "05", "Исходящие документы",  "Исходящий документ",    "Вам поступил "),
-    SECRET_INCOMING_DOCUMENTS (  6, "06", "Входящие секретные",   "Входящий секретный",    "Вам поступил "),
-    SECRET_INCOMING_ORDERS    (  7, "07", "НПА секретные",        "НПА секретный",         "Вам поступил "),
-    SECRET_ORDERS             (  8, "08", "Пр. старые секретные", "Пр. старый секретный",  "Вам поступил "),
-    SECRET_OUTGOING_DOCUMENTS (  9, "09", "Исходящие секретные",  "Исходящий секретный",   "Вам поступил "),
-    ORDERS_DDO                ( 10, "10", "Приказы",              "Приказ",                "Вам поступил "),
-    CONTROL                   ( 97, "97", "На контроле",          " ",                     "Вам поступил "),
-    SIGN                      ( 98, "98", "Подписание",           "Подписание",            "Вам поступил документ на "),
-    APPROVE                   ( 99, "99", "Согласование",         "Согласование",          "Вам поступил документ на ");
+    ALL_JOURNALS              ( "90", " ",                     ""),
+    CITIZEN_REQUESTS          ( "01", "Обращение граждан",     "Вам поступило "),
+    INCOMING_DOCUMENTS        ( "02", "Входящий документ",     "Вам поступил "),
+    INCOMING_ORDERS           ( "03", "НПА",                   "Вам поступил "),
+    ORDERS                    ( "04", "Приказ старый",         "Вам поступил "),
+    OUTGOING_DOCUMENTS        ( "05", "Исходящий документ",    "Вам поступил "),
+    SECRET_INCOMING_DOCUMENTS ( "06", "Входящий секретный",    "Вам поступил "),
+    SECRET_INCOMING_ORDERS    ( "07", "НПА секретный",         "Вам поступил "),
+    SECRET_ORDERS             ( "08", "Пр. старый секретный",  "Вам поступил "),
+    SECRET_OUTGOING_DOCUMENTS ( "09", "Исходящий секретный",   "Вам поступил "),
+    ORDERS_DDO                ( "10", "Приказ",                "Вам поступил "),
+    CONTROL                   ( "97", " ",                     "Вам поступил "),
+    SIGN                      ( "98", "Подписание",            "Вам поступил документ на "),
+    APPROVE                   ( "99", "Согласование",          "Вам поступил документ на ");
 
     private final String value;
-    private final String name;
     private final String single;
-    private final Integer type;
     private final String formattedName;
 
-    Journal(final int index, final String value, final String name, final String single, final  String formattedName) {
-      this.type  = index;
+    Journal(final String value, final String single, final  String formattedName) {
       this.value = value;
-      this.name  = name;
       this.single  = single;
       this.formattedName = formattedName;
     }
@@ -40,14 +34,8 @@ public class Fields {
       return formattedName;
     }
 
-    public Integer getType(){
-      return type;
-    }
     public String getValue(){
       return value;
-    }
-    public String getName() {
-      return name;
     }
     public String getSingle() {
       return single;
@@ -60,25 +48,23 @@ public class Fields {
   }
 
   public enum Menu {
-    ALL                ( 0  , "ALL"                , "Документы / Проекты" ),
-    INCOMING_DOCUMENTS ( 1  , "INCOMING_DOCUMENTS" , "Входящие документы" ),
-    CITIZEN_REQUESTS   ( 2  , "CITIZEN_REQUESTS"   , "Обращения граждан" ),
-    APPROVE_ASSIGN     ( 3  , "APPROVE_ASSIGN"     , "Подписание/Согласование" ),
-    INCOMING_ORDERS    ( 4  , "INCOMING_ORDERS"    , "НПА" ),
-    ORDERS             ( 5  , "ORDERS"             , "Приказы" ),
-    ORDERS_DDO         ( 6  , "ORDERS_DDO"         , "Приказы ДДО" ),
-    IN_DOCUMENTS       ( 7  , "IN_DOCUMENTS"       , "Внутренние документы" ),
-    ON_CONTROL         ( 8  , "ON_CONTROL"         , "На контроле" ),
-    PROCESSED          ( 9  , "PROCESSED"          , "Обработанное" ),
-    FAVORITES          ( 10 , "FAVORITES"          , "Избранное" );
+    ALL                ( 0  , "Документы / Проекты" ),
+    INCOMING_DOCUMENTS ( 1  , "Входящие документы" ),
+    CITIZEN_REQUESTS   ( 2  , "Обращения граждан" ),
+    APPROVE_ASSIGN     ( 3  , "Подписание/Согласование" ),
+    INCOMING_ORDERS    ( 4  , "НПА" ),
+    ORDERS             ( 5  , "Приказы" ),
+    ORDERS_DDO         ( 6  , "Приказы ДДО" ),
+    IN_DOCUMENTS       ( 7  , "Внутренние документы" ),
+    ON_CONTROL         ( 8  , "На контроле" ),
+    PROCESSED          ( 9  , "Обработанное" ),
+    FAVORITES          ( 10 , "Избранное" );
 
     private final Integer index;
-    private final String value;
     private final String title;
 
-    Menu(final int index, final String value, final String title) {
+    Menu(final int index, final String title) {
       this.index  = index;
-      this.value = value;
       this.title  = title;
     }
 
@@ -86,17 +72,8 @@ public class Fields {
       return index;
     }
 
-    public String getValue() {
-      return value;
-    }
-
     public String getTitle() {
       return title;
-    }
-
-    @Override
-    public String toString() {
-      return value;
     }
 
     public static Menu getMenu( String string){
