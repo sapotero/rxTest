ae3510349b35fb69007ab8e62c047e96421f507a
Gennady Petukhov <gpetukhov@n-core.ru>
2017-10-03 13:40:28 +0300

    Set red label in AddDecision

--- app/src/main/java/sapotero/rxtest/managers/menu/commands/decision/AddDecision.java
+++ app/src/main/java/sapotero/rxtest/managers/menu/commands/decision/AddDecision.java
@@ -3,13 +3,17 @@ package sapotero.rxtest.managers.menu.commands.decision;
 import org.greenrobot.eventbus.EventBus;
 
 import java.util.List;
+import java.util.Objects;
 
 import rx.Observable;
+import sapotero.rxtest.db.requery.models.RDocumentEntity;
+import sapotero.rxtest.db.requery.models.RManagerEntity;
 import sapotero.rxtest.events.document.UpdateDocumentEvent;
 import sapotero.rxtest.managers.menu.commands.DecisionCommand;
 import sapotero.rxtest.managers.menu.utils.CommandParams;
 import sapotero.rxtest.retrofit.models.document.Decision;
 import sapotero.rxtest.retrofit.models.v2.DecisionError;
+import sapotero.rxtest.utils.memory.models.InMemoryDocument;
 import timber.log.Timber;
 
 public class AddDecision extends DecisionCommand {
@@ -24,6 +28,8 @@ public class AddDecision extends DecisionCommand {
 
   @Override
   public void execute() {
+    setRedLabel();
+
     getParams().getDecisionModel().setApproved( false );
     createTemporaryDecision();
 
@@ -33,6 +39,38 @@ public class AddDecision extends DecisionCommand {
 
     queueManager.add(this);
     setAsProcessed();
+  }
+
+  // resolved https://tasks.n-core.ru/browse/MPSED-2206
+  // Проставлять признак red у документа, при создании/подписании резолюции
+  private boolean setRedLabel() {
+    boolean result = false;
+
+    int count = dataStore
+      .count( RManagerEntity.class )
+      .where( RManagerEntity.USER.eq( getParams().getLogin() ) )
+      .and( RManagerEntity.UID.eq( getParams().getDecisionModel().getSignerId() ) )
+      .get().value();
+
+    // Если подписант министр и подписант не равен текущему пользователю (т.е. текущий пользователь не министр)
+    if ( count > 0 && !Objects.equals( getParams().getDecisionModel().getSignerId(), getParams().getCurrentUserId() ) ) {
+      getParams().getDecisionModel().setRed( true );
+
+      InMemoryDocument inMemoryDocument = store.getDocuments().get( getParams().getDocument() );
+      if ( inMemoryDocument != null ) {
+        inMemoryDocument.getDocument().setRed( true );
+      }
+
+      dataStore
+        .update( RDocumentEntity.class )
+        .set( RDocumentEntity.RED, true )
+        .where( RDocumentEntity.UID.eq( getParams().getDocument() ) )
+        .get().value();
+
+      result = true;
+    }
+
+    return result;
   }
 
   @Override
